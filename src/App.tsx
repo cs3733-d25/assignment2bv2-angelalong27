f86c9b29793a8cce00a5005ed32c6d7954f6f310
thakura1 <athakur1@wpi.edu>
2025-04-01 15:18:39 -0400

    Added Additional tsx files for second hobby

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Heading from "./props/title"
+import Title from "./props/title.tsx"
 
 
 function App() {
 
   return (
     <>
-      <Heading title={"CS3733-D25 Team 13: Dancing and Cooking"}/>
+      <Title title={"CS3733-D25 Team 13: Dancing and Cooking"}/>
     </>
   )
 }
