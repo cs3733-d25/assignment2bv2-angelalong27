12b16e7ad70f7f0aa4618fa24d7f0050855f862a
thakura1 <athakur1@wpi.edu>
2025-04-02 21:39:46 -0400

    merged Angela new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
 // import Heading from "./props/title"
-import CuisineList from './props/list2';
-import { FillForm2 } from './props/form2';
 import Hobby2 from "./props/hobby2.tsx";
 import Title from "./props/title.tsx";
 import Intro from "./props/intro.tsx";
 import Hobby1 from "./props/hobby1.tsx";
-import Dancer from "./assets/indianDance.jpg";
+
 
 
 function App() {
@@ -15,11 +13,8 @@ function App() {
     <>
       <Title title={"CS3733-D25 Team 13: Dancing and Cooking"}/>
         <Intro name1={"Aditri"} name2={"Angela"}/>
-      <Hobby2 />
-      <CuisineList />
-      <FillForm2 />
-      <img src={Dancer} alt="dancer" width="360"/>
-      <Hobby1 />
+        <Hobby1 />
+        <Hobby2 />
     </>
   )
 }
--- src/props/hobby1.tsx
+++ src/props/hobby1.tsx
@@ -1,11 +1,17 @@
 import {FillForm} from "./form1.tsx";
 import List1 from "./list1.tsx";
 import Table1 from "./table1.tsx";
+import Dance from '../../public/indianDance.jpg';
 
 function Hobby1() {
 
     return (
         <>
+            <br/>
+            <p>This section is about my hobby of Dancing. I've been dancing since I was 10 years old.
+                The first dance form I learned was Bharatanatyam or classical Indian dance. I've also been apart of
+                the competitive dance group here at WPI, Rangeela.</p>
+            <img src={ Dance } alt="dancer" width="360" height="120" />
             <List1/>
             <Table1/>
             <FillForm/>
